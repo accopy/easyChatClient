22cb53ed56249de315f7d8a06d82ae659464671e
9527 <maoguge161@gmail.com>
2023-11-22 13:58:28 +0800

    first

---
+++ src/views/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container">
+        hellosssssssssssssss
+    </div>
+</template>
+
+<script setup>
+import { reactive, toRefs, onBeforeMount, onMounted } from 'vue'
+const data = reactive({
+    name: '',
+})
+
+</script>
+<style scoped lang='scss'>
+.container {
+    background-color: $dark-bg-color;
+    width: 100%;
+    height: 100vh;
+}
+</style>
---
+++ vite.config.js
@@ -0,0 +1,33 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path';
+//vue-plugin-Pages自动配置路由
+import Pages from 'vite-plugin-pages'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    Pages({
+      dirs: 'src/views',  // 需要生成路由的文件目录
+      exclude: ['**/components/*.vue']  // 排除在外的目录，即不将所有 components 目录下的 .vue 文件生成路由
+    })
+  ],
+  resolve: {
+    // 配置路径别名
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        //scss文件预处理
+        additionalData: `@import "@/assets/theme/theme.scss";`
+      }
+    }
+  }
+
+})
+
+
